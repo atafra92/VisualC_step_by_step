8164079a47c1fe2f33d3247efda3f6abbb16b01a
Antonio Tafra <antonio.tafra@gmail.com>
2021-01-17 17:00:01 +0100

    Using inheritance and virtual methods
    practising inheritance OOP from base class to derived classes, using virtual keyword to override base class methods

---
+++ Vehicles/Vehicles/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine("Journey by airplane:");
+            Airplane myPlane = new Airplane();
+            myPlane.StartEngine("Conctact");
+            myPlane.TakeOff(); 
+            myPlane.Drive(); //Drive() method in base class Vehicle and ovveriden method are not the same 
+            myPlane.Land();
+            myPlane.StopEngine("Whirr");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Journey by car:");
+            Car myCar = new Car();
+            myCar.StartEngine("brm brm");
+            myCar.Accelerate();
+            myCar.Drive(); //Drive() method in base class Vehicle and ovveriden method are not the same 
+            myCar.Brake();
+            myCar.StopEngine("Phut phut");
+
+            Console.WriteLine("\nTesting polymorphism");
+            Vehicle v = myCar; //creating a reference to Car object by using a Vehicle variable
+            v.Drive(); //calls Drive() method by using Vehicle variable (which is Motoring)
+            v = myPlane;
+            v.Drive(); // this is Flying 
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vehicles/Vehicles/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public class Vehicle
+    {
+        public void StartEngine(string noiseToMakeWhenStarting)
+        {
+            Console.WriteLine($"Starting engine...");
+        }
+
+        public void StopEngine(string noiseToMakeWhenStopping)
+        {
+            Console.WriteLine($"Stopping engine...");
+        }
+
+        /// <summary>
+        /// This method should be virtual if we want to override it in some of derived classes 
+        /// </summary>
+        public virtual void Drive()
+        {
+            Console.WriteLine("Default implementation of the Drive method");
+        }
+    }
+}
