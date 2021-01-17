cc81c5641588214343afd5b4da932cee5b5cd763
Antonio Tafra <antonio.tafra@gmail.com>
2021-01-17 15:44:19 +0100

    Using params keyword with arrays
    use case of params with value type that takes any number of parameters of same type and params object[] that takes any number of parameters of different types

---
+++ ParamsArray/ParamsArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    class Program
+    {
+        static void doWork()
+        {
+            //Console.WriteLine(Util.Sum(null));
+            //Console.WriteLine(Util.Sum());
+
+            //Console.WriteLine(Util.Sum(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
+
+            //at runtime overload of Sum method it called because it is most closely matches the method call
+            Console.WriteLine(Util.Sum(2, 4, 6, 8)); //outputs 20
+
+            //clr uses method overload which accepts optional parameters once more - it has precedence before params int[] parameter 
+            Console.WriteLine(Util.Sum(2, 4, 6)); //outputs 12
+
+            //this time there is 5 parameters passed in so CLR takes parameters list overload of Sum method 
+            Console.WriteLine(Util.Sum(2, 4, 6, 8, 10)); //outputs 30
+
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+                
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
