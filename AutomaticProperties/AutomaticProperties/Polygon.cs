914924c3c33740f3ce7b25d62c2323372673a437
Antonio Tafra <antonio.tafra@gmail.com>
2021-01-18 20:52:20 +0100

    Implementing properties
    Declaring interface properties,generating  automatic properties, using properties

---
+++ AutomaticProperties/AutomaticProperties/Polygon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticProperties
+{
+    public class Polygon
+    {
+        public int NumSides { get; set; }
+        public double SideLenght { get; set; }
+
+        /// <summary>
+        /// constructor initializes properties with default values 
+        /// </summary>
+        public Polygon()
+        {
+            NumSides = 4;
+            SideLenght = 10.0;
+        }
+    }
+}
---
+++ AutomaticProperties/AutomaticProperties/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomaticProperties
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Polygon square = new Polygon();
+            Polygon triange = new Polygon()
+            {
+                NumSides = 3
+                //SideLenght is set to 10.0 in a class constructor 
+            };
+            Polygon pentagon = new Polygon
+            {
+                SideLenght = 15.5,
+                NumSides = 5
+            };
+
+            Console.WriteLine($"Square: number of sides is {square.NumSides }, length of each side is { square.SideLenght }");
+            Console.WriteLine($"Triangle: number of sides is { triange.NumSides }, length of each side is { triange.SideLenght }");
+            Console.WriteLine($"Pentagon: number of sides is { pentagon.NumSides }, length of each side is { pentagon.SideLenght }");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
