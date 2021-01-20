61ade85e35e0dde0399b0496aab229aa867bb19a
Antonio Tafra <antonio.tafra@gmail.com>
2021-01-20 13:42:15 +0100

    Uising a ToList() method with IEnumerable<T>

--- QueryBinaryTree/QueryBinaryTree/Program.cs
+++ QueryBinaryTree/QueryBinaryTree/Program.cs
@@ -20,57 +20,76 @@ namespace QueryBinaryTree
             empTree.Insert(new Employee { Id = 3, FirstName = "Deni", LastName = "Juras", Department = "Sales" });
             empTree.Insert(new Employee { Id = 5, FirstName = "Grgo", LastName = "Tafra", Department = "Marketing" });
 
-            Console.WriteLine("List of departments");
-            var depts = empTree.Select(d => d.Department); //select method returns entire row 
+            Console.WriteLine("All employees");
+            var allEmployees = from e in empTree/*.ToList()  - cache the data and retreive it's copy when iterating over a collection*/ 
+                               select e;
 
-            // var deptsDistinct = empTree.Select(d => d.Department).Distinct(); 
-            //using select...from statement
-            var deptsDistinct = (from d in empTree
-                        select d.Department).Distinct();
-
-            foreach (var dept in deptsDistinct)
+            foreach (var emp in allEmployees)
             {
-                Console.WriteLine($"Department: {dept}"); //orders list by Id because of CompareTo method in Employee class
-                Console.WriteLine($"Distinct Departments: {dept}");
+                Console.WriteLine(emp);
             }
-
+            //add a new employee to a IEnumerable<T> allEmployees
+            empTree.Insert(new Employee { Id = 7, FirstName = "Ante", LastName = "Martic", Department = "IT" });
             Console.WriteLine();
-            Console.WriteLine("Employyes in IT Department:");
-            //var employeesInIT = empTree.Where(e => e.Department == "IT").Select(e => e); //..ILI
-            //var employeesInIt = empTree.Where(e => String.Equals(e.Department, "IT")).Select(e => e);
+            Console.WriteLine("Employee added");
 
-            //using select...from
-            var employeesInIT = from emp in empTree
-                                where String.Equals(emp.Department, "IT")
-                                select emp;
-
-            foreach (var emp in employeesInIT)
+            Console.WriteLine("All employees");
+            foreach (var emp in allEmployees)
             {
                 Console.WriteLine(emp);
             }
 
-            //Group the employees found in the binary tree by department. The outer foreach loop iterates 
-            //throught each group, displaying name of department. The inner foreach displays the names of emmployees 
-            //in each department
-            Console.WriteLine();
-            Console.WriteLine("All employees grouped by department:");
+            //Console.WriteLine("List of departments");
+            //var depts = empTree.Select(d => d.Department); //select method returns entire row 
 
-            //var employeesByDept = empTree.GroupBy(e => e.Department);
+            //// var deptsDistinct = empTree.Select(d => d.Department).Distinct(); 
+            ////using select...from statement
+            //var deptsDistinct = (from d in empTree
+            //            select d.Department).Distinct();
 
-            //using select...from
-            var employeesByDept = from dept in empTree
-                                  group dept by dept.Department;
+            //foreach (var dept in deptsDistinct)
+            //{
+            //    Console.WriteLine($"Department: {dept}"); //orders list by Id because of CompareTo method in Employee class
+            //    Console.WriteLine($"Distinct Departments: {dept}");
+            //}
 
-            foreach (var dept in employeesByDept)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"Department: { dept.Key }");
+            //Console.WriteLine();
+            //Console.WriteLine("Employyes in IT Department:");
+            ////var employeesInIT = empTree.Where(e => e.Department == "IT").Select(e => e); //..ILI
+            ////var employeesInIt = empTree.Where(e => String.Equals(e.Department, "IT")).Select(e => e);
 
-                foreach (var emp in dept)
-                {
-                    Console.WriteLine($"{ emp.FirstName } { emp.LastName }");
-                }
-            }
+            ////using select...from
+            //var employeesInIT = from emp in empTree
+            //                    where String.Equals(emp.Department, "IT")
+            //                    select emp;
+
+            //foreach (var emp in employeesInIT)
+            //{
+            //    Console.WriteLine(emp);
+            //}
+
+            ////Group the employees found in the binary tree by department. The outer foreach loop iterates 
+            ////throught each group, displaying name of department. The inner foreach displays the names of emmployees 
+            ////in each department
+            //Console.WriteLine();
+            //Console.WriteLine("All employees grouped by department:");
+
+            ////var employeesByDept = empTree.GroupBy(e => e.Department);
+
+            ////using select...from
+            //var employeesByDept = from dept in empTree
+            //                      group dept by dept.Department;
+
+            //foreach (var dept in employeesByDept)
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine($"Department: { dept.Key }");
+
+            //    foreach (var emp in dept)
+            //    {
+            //        Console.WriteLine($"{ emp.FirstName } { emp.LastName }");
+            //    }
+            //}
 
         }
 
